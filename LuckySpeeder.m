f51f4596f8c13e0945d1f709ae7288b42ebe1873
kekeimiku <kekelanact@gmail.com>
2024-12-07 23:18:32 +0800

    add zznq

--- LuckySpeeder.m
+++ LuckySpeeder.m
@@ -3,6 +3,7 @@
 MIT License
 
 Copyright (c) 2024 kekeimiku
+Copyright (c) 2024 ac0d3r
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
